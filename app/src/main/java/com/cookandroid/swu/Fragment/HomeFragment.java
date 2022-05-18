40787259689a92390fe92bfcb079c0e07fae4b1d
Wonahyeon <wonah10220@gmail.com>
2022-05-19 03:17:24 +0900

    다이얼로그

--- app/src/main/java/com/cookandroid/swu/Fragment/HomeFragment.java
+++ app/src/main/java/com/cookandroid/swu/Fragment/HomeFragment.java
@@ -1,7 +1,15 @@
 package com.cookandroid.swu.Fragment;
 
+import android.app.AlertDialog;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,19 +17,48 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.cookandroid.swu.MainActivity;
 import com.cookandroid.swu.R;
 
-
 public class HomeFragment extends Fragment {
-    ImageButton ImgBin;
-
+    Context context;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+    public void onViewCreated(@NonNull View view, Bundle savedInstancedState){
+        super.onViewCreated(view,savedInstancedState);
+        context = ((MainActivity)getActivity()).getApplicationContext();
+        view.findViewById(R.id.ImgBin).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                 show();
+            }
+        });
+    }
+    void show(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("우리집 구급함");
+        builder.setMessage("약을 버렸나요?");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(context,"확인되었습니다.",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Toast.makeText(context,"우리집 구급함으로 이동합니다.",Toast.LENGTH_SHORT).show();
+                // 우리집 구급함으로 이동
+            }
+        });
+        builder.show();
+    }
+
 
 
 }
--- app/src/main/java/com/cookandroid/swu/MainActivity.java
+++ app/src/main/java/com/cookandroid/swu/MainActivity.java
@@ -1,16 +1,11 @@
 package com.cookandroid.swu;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
 import com.cookandroid.swu.Fragment.EboxFragment;
 import com.cookandroid.swu.Fragment.HomeFragment;
@@ -20,13 +15,7 @@ import com.cookandroid.swu.Fragment.SetFragment;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
-    Fragment HomeFragment;
-    Fragment SearchFragment;
-    Fragment EboxFragment;
-    Fragment PlistFragment;
-    Fragment SetFragment;
-    ImageButton ImgBin;
+    Fragment HomeFragment,SearchFragment,EboxFragment,PlistFragment,SetFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,35 +54,7 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-        //팝업창
-        ImgBin = findViewById(R.id.ImgBin);
-        ImgBin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //팝업 알림 생성
-                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
-
-                dlg.setTitle("우리집 구급함");    //제목
-                dlg.setMessage("유통기한 지난 약이 존재합니다. 버리셨나요?"); //메시지
-                //dlg.setIcon(R.drawable.ebox);   //아이콘 설정
-
-                //클릭시 동작
-                //버튼 설정
-                //ok 눌렀을 때
-                dlg.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // 토스트 메시지
-                        Toast.makeText(MainActivity.this,"확인되었습니다.",Toast.LENGTH_SHORT).show();
-                    }
-                    //no 눌렀을 때
-                });
-                //dlg.setCancelable(false);   //팝업 떴을 때 뒤로가기 버튼 x
-                dlg.show(); //팝업 띄우기
-            }
-        });
-
-
-
     }
+
+
 }
