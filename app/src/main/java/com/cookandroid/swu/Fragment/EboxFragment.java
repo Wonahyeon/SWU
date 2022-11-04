495f57b95219603f94c024470e8f70df9fa7e008
xllxx <80588214+xllxx@users.noreply.github.com>
2022-11-05 07:10:55 +0900

    1105 (no camera)

--- app/src/main/java/com/cookandroid/swu/Fragment/EboxFragment.java
+++ app/src/main/java/com/cookandroid/swu/Fragment/EboxFragment.java
@@ -1,5 +1,6 @@
 package com.cookandroid.swu.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,17 +8,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.cookandroid.swu.EboxAdd;
 import com.cookandroid.swu.R;
 
 
 public class EboxFragment extends Fragment {
-
+Button addbutton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ebox, container, false);
+        View v = inflater.inflate(R.layout.fragment_ebox, container, false);
+
+        addbutton =(Button) v.findViewById(R.id.addbtn);
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), EboxAdd.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 }
