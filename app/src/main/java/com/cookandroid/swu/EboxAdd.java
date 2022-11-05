51c1df91c13376f53192a601487aa5e3e1c1b0d7
xllxx <80588214+xllxx@users.noreply.github.com>
2022-11-05 17:41:41 +0900

    EboxAdd완료

--- app/src/main/java/com/cookandroid/swu/EboxAdd.java
+++ app/src/main/java/com/cookandroid/swu/EboxAdd.java
@@ -1,17 +1,51 @@
 package com.cookandroid.swu;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.util.IOUtils;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class EboxAdd extends AppCompatActivity {
 
@@ -24,30 +58,44 @@ public class EboxAdd extends AppCompatActivity {
     int mMonth= c.get(Calendar.MONTH);
     int mDay = c.get(Calendar.DAY_OF_MONTH);
 
+    final int CAMERA = 100; // 카메라 선택시 인텐트로 보내는 값
+    final int GALLERY = 101; // 갤러리 선택 시 인텐트로 보내는 값
+    Intent intent;
+    @SuppressLint("SimpleDateFormat")
+    SimpleDateFormat imageDate = new SimpleDateFormat("yyyyMMdd_HHmmss");
+    String imagePath;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ebox_add);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //뒤로가기 버튼
 
+        //접근 확인
+        boolean hasCamPerm = checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+        boolean hasWritePerm = checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        if (!hasCamPerm || !hasWritePerm)  // 권한 없을 시  권한설정 요청
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
         view1 = (ImageView) findViewById(R.id.ebox_view);
         dateview = (Button) findViewById(R.id.datebtn);
 
         //camera
         view1.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("QueryPermissionsNeeded")
             @Override
             public void onClick(View view) {
                 DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int which) {
-                        //doTakePhotoAction();
+                        doTakePhotoAction();
                     }
                 };
                 DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int which) {
-                        // doTakeAlbumAction();
+                        doTakeAlbumAction();
                     }
                 };
                 DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
@@ -85,4 +133,77 @@ public class EboxAdd extends AppCompatActivity {
             }
         }); //setonclicklistener
     }//oncreate
+
+    private void doTakePhotoAction()
+    {
+        intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            File imageFile = null;
+            try {
+                imageFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (imageFile != null) {
+                Uri imageUri = FileProvider.getUriForFile(getApplicationContext(),
+                        "com.example.getimage.fileprovider",
+                        imageFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, CAMERA);
+            }
+        }
+    }
+
+    private void doTakeAlbumAction()
+    {
+        intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY);
+    }
+
+    File createImageFile() throws IOException {
+//        이미지 파일 생성
+        String timeStamp = imageDate.format(new Date()); // 파일명 중복을 피하기 위한 "yyyyMMdd_HHmmss"꼴의 timeStamp
+        String fileName = "IMAGE_" + timeStamp; // 이미지 파일 명
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File file = File.createTempFile(fileName, ".jpg", storageDir); // 이미지 파일 생성
+        imagePath = file.getAbsolutePath(); // 파일 절대경로 저장하기
+        return file;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) { // 결과가 있을 경우
+            Bitmap bitmap = null;
+            switch (requestCode) {
+                case GALLERY: // 갤러리에서 이미지로 선택한 경우
+                    Cursor cursor = getContentResolver().query(data.getData(), null, null, null, null);
+                    if (cursor != null) {
+                        cursor.moveToFirst();
+                        int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                        imagePath = cursor.getString(index);
+                        bitmap = BitmapFactory.decodeFile(imagePath);
+                        cursor.close();
+                    }
+//                    InputStream 으로 이미지 세팅하기
+                    try {
+                        InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                        bitmap = BitmapFactory.decodeStream(inputStream);
+                        inputStream.close();
+                        view1.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case CAMERA: // 카메라로 이미지 가져온 경우
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inSampleSize = 2; // 이미지 축소 정도. 원 크기에서 1/inSampleSize 로 축소됨
+                    bitmap = BitmapFactory.decodeFile(imagePath, options);
+                    break;
+            }
+            view1.setImageBitmap(bitmap);
+        }
+    }
 }//class
