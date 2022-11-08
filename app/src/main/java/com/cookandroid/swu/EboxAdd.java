d43460b1343ff3e263e290852cf224de2e11b0e8
xllxx <80588214+xllxx@users.noreply.github.com>
2022-11-09 04:58:49 +0900

    EboxFragment Listview, Calling Fragment method from Activity (22.01.09)

--- app/src/main/java/com/cookandroid/swu/EboxAdd.java
+++ app/src/main/java/com/cookandroid/swu/EboxAdd.java
@@ -31,9 +31,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.cookandroid.swu.Fragment.EboxFragment;
 import com.google.android.gms.common.util.IOUtils;
 
 import java.io.File;
@@ -49,8 +51,14 @@ import java.util.Date;
 
 public class EboxAdd extends AppCompatActivity {
 
+
+    private Button savebtn;
     private ImageView view1;
     private Button dateview;
+    File photoFile;
+    Bitmap photo;
+    static String ebox_date, ebox_name, ebox_sympton, ebox_memo;
+    private EditText eboxname, eboxsympton,eboxmemo;
 
     //날짜 선택
     private Calendar c = Calendar.getInstance();
@@ -59,11 +67,9 @@ public class EboxAdd extends AppCompatActivity {
     int mDay = c.get(Calendar.DAY_OF_MONTH);
 
     final int CAMERA = 100; // 카메라 선택시 인텐트로 보내는 값
-    final int GALLERY = 101; // 갤러리 선택 시 인텐트로 보내는 값
-    Intent intent;
+    final int GALLERY = 300; // 갤러리 선택 시 인텐트로 보내는 값
+
     @SuppressLint("SimpleDateFormat")
-    SimpleDateFormat imageDate = new SimpleDateFormat("yyyyMMdd_HHmmss");
-    String imagePath;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -71,6 +77,10 @@ public class EboxAdd extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ebox_add);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //뒤로가기 버튼
+
+        EboxFragment tf = (EboxFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+
+
 
         //접근 확인
         boolean hasCamPerm = checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
@@ -80,6 +90,10 @@ public class EboxAdd extends AppCompatActivity {
 
         view1 = (ImageView) findViewById(R.id.ebox_view);
         dateview = (Button) findViewById(R.id.datebtn);
+        savebtn =(Button)findViewById(R.id.ebox_save);
+        eboxname=(EditText)findViewById(R.id.ebox_name);
+        eboxmemo=(EditText)findViewById(R.id.ebox_memo);
+        eboxsympton=(EditText)findViewById(R.id.ebox_sympton);
 
         //camera
         view1.setOnClickListener(new View.OnClickListener() {
@@ -118,7 +132,9 @@ public class EboxAdd extends AppCompatActivity {
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
                 dateview.setText(year + " - " + (month + 1) + " - " + dayOfMonth);
+                ebox_date = String.format("유통기한 : " + year + " - " + (month + 1) + " - " + dayOfMonth);
             }
         }, mYear, mMonth, mDay);
 
@@ -132,78 +148,332 @@ public class EboxAdd extends AppCompatActivity {
                 }
             }
         }); //setonclicklistener
+
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ebox_name = eboxname.getText().toString();
+                ebox_memo = eboxmemo.getText().toString();
+                ebox_sympton = eboxsympton.getText().toString();
+
+                tf.addItem(photo,ebox_name,ebox_sympton);
+                finish();
+            }
+        });
     }//oncreate
 
     private void doTakePhotoAction()
     {
-        intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            File imageFile = null;
-            try {
-                imageFile = createImageFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (imageFile != null) {
-                Uri imageUri = FileProvider.getUriForFile(getApplicationContext(),
-                        "com.example.getimage.fileprovider",
-                        imageFile);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                EboxAdd.this, android.Manifest.permission.CAMERA);
+
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(EboxAdd.this,
+                    new String[]{android.Manifest.permission.CAMERA} ,
+                    1000);
+            Toast.makeText(EboxAdd.this, "카메라 권한 필요합니다.",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(intent.resolveActivity(getPackageManager())  != null  ){
+
+                // 사진의 파일명을 만들기
+                String fileName = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                photoFile = getPhotoFile(fileName);
+
+                Uri fileProvider = FileProvider.getUriForFile(EboxAdd.this,
+                        "com.cookandroid.swu.fileprovider", photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
                 startActivityForResult(intent, CAMERA);
+
+            } else{
+                Toast.makeText(EboxAdd.this, "이폰에는 카메라 앱이 없습니다.",
+                        Toast.LENGTH_SHORT).show();
             }
         }
     }
 
+    private File getPhotoFile(String fileName) {
+        File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        try{
+            return File.createTempFile(fileName, ".jpg", storageDirectory);
+        }catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
     private void doTakeAlbumAction()
     {
-        intent = new Intent(Intent.ACTION_PICK);
-        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-        intent.setType("image/*");
-        startActivityForResult(intent, GALLERY);
+        if(checkPermission()){
+            displayFileChoose();
+        }else{
+            requestPermission();
+        }
     }
+    private void requestPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(EboxAdd.this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+            Toast.makeText(EboxAdd.this, "권한 수락이 필요합니다.",
+                    Toast.LENGTH_SHORT).show();
+        }else{
+            ActivityCompat.requestPermissions(EboxAdd.this,
+                    new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 500);
+        }
+    }
+    private boolean checkPermission(){
+        int result = ContextCompat.checkSelfPermission(EboxAdd.this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(result == PackageManager.PERMISSION_DENIED){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    private void displayFileChoose() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, GALLERY);
 
-    File createImageFile() throws IOException {
-//        이미지 파일 생성
-        String timeStamp = imageDate.format(new Date()); // 파일명 중복을 피하기 위한 "yyyyMMdd_HHmmss"꼴의 timeStamp
-        String fileName = "IMAGE_" + timeStamp; // 이미지 파일 명
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File file = File.createTempFile(fileName, ".jpg", storageDir); // 이미지 파일 생성
-        imagePath = file.getAbsolutePath(); // 파일 절대경로 저장하기
-        return file;
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) { // 결과가 있을 경우
-            Bitmap bitmap = null;
-            switch (requestCode) {
-                case GALLERY: // 갤러리에서 이미지로 선택한 경우
-                    Cursor cursor = getContentResolver().query(data.getData(), null, null, null, null);
-                    if (cursor != null) {
-                        cursor.moveToFirst();
-                        int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-                        imagePath = cursor.getString(index);
-                        bitmap = BitmapFactory.decodeFile(imagePath);
-                        cursor.close();
-                    }
-//                    InputStream 으로 이미지 세팅하기
-                    try {
-                        InputStream inputStream = getContentResolver().openInputStream(data.getData());
-                        bitmap = BitmapFactory.decodeStream(inputStream);
-                        inputStream.close();
-                        view1.setImageBitmap(bitmap);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case CAMERA: // 카메라로 이미지 가져온 경우
-                    BitmapFactory.Options options = new BitmapFactory.Options();
-                    options.inSampleSize = 2; // 이미지 축소 정도. 원 크기에서 1/inSampleSize 로 축소됨
-                    bitmap = BitmapFactory.decodeFile(imagePath, options);
-                    break;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1000: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(EboxAdd.this, "권한 허가 되었음",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(EboxAdd.this, "아직 승인하지 않았음",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
             }
-            view1.setImageBitmap(bitmap);
+            case 500: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(EboxAdd.this, "권한 허가 되었음",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(EboxAdd.this, "아직 승인하지 않았음",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
         }
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 100 && resultCode == RESULT_OK){
+
+            photo = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+
+            ExifInterface exif = null;
+            try {
+                exif = new ExifInterface(photoFile.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED);
+            photo = rotateBitmap(photo, orientation);
+
+            // 압축시킨다. 해상도 낮춰서
+            OutputStream os;
+            try {
+                os = new FileOutputStream(photoFile);
+                photo.compress(Bitmap.CompressFormat.JPEG, 50, os);
+                os.flush();
+                os.close();
+            } catch (Exception e) {
+                Log.e(getClass().getSimpleName(), "Error writing bitmap", e);
+            }
+
+            photo = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+
+            view1.setImageBitmap(photo);
+            view1.setScaleType(ImageView.ScaleType.FIT_XY);
+
+            // 네트워크로 데이터 보낸다.
+
+
+
+        }else if(requestCode == 300 && resultCode == RESULT_OK && data != null &&
+                data.getData() != null){
+
+            Uri albumUri = data.getData( );
+            String fileName = getFileName( albumUri );
+            try {
+
+                ParcelFileDescriptor parcelFileDescriptor = getContentResolver( ).openFileDescriptor( albumUri, "r" );
+                if ( parcelFileDescriptor == null ) return;
+                FileInputStream inputStream = new FileInputStream( parcelFileDescriptor.getFileDescriptor( ) );
+                photoFile = new File( this.getCacheDir( ), fileName );
+                FileOutputStream outputStream = new FileOutputStream( photoFile );
+                IOUtils.copyStream( inputStream, outputStream );
+
+//                //임시파일 생성
+//                File file = createImgCacheFile( );
+//                String cacheFilePath = file.getAbsolutePath( );
+
+
+                // 압축시킨다. 해상도 낮춰서
+                 photo = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                OutputStream os;
+                try {
+                    os = new FileOutputStream(photoFile);
+                    photo.compress(Bitmap.CompressFormat.JPEG, 60, os);
+                    os.flush();
+                    os.close();
+                } catch (Exception e) {
+                    Log.e(getClass().getSimpleName(), "Error writing bitmap", e);
+                }
+               // InputStream inputStream1 = getContentResolver().openInputStream(albumUri);
+                //photo = BitmapFactory.decodeStream(inputStream1);
+                //inputStream.close();
+                view1.setImageBitmap(photo);
+                view1.setScaleType(ImageView.ScaleType.FIT_XY);
+
+//                imageView.setImageBitmap( getBitmapAlbum( imageView, albumUri ) );
+
+            } catch ( Exception e ) {
+                e.printStackTrace( );
+            }
+
+            // 네트워크로 보낸다.
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    public static Bitmap rotateBitmap(Bitmap bitmap, int orientation) {
+
+        Matrix matrix = new Matrix();
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_NORMAL:
+                return bitmap;
+            case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+                matrix.setScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                matrix.setRotate(180);
+                break;
+            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+                matrix.setRotate(180);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_TRANSPOSE:
+                matrix.setRotate(90);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.setRotate(90);
+                break;
+            case ExifInterface.ORIENTATION_TRANSVERSE:
+                matrix.setRotate(-90);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                matrix.setRotate(-90);
+                break;
+            default:
+                return bitmap;
+        }
+        try {
+            Bitmap bmRotated = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            bitmap.recycle();
+            return bmRotated;
+        }
+        catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public String getFileName( Uri uri ) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        try {
+            if (cursor == null) return null;
+            cursor.moveToFirst();
+            @SuppressLint("Range") String fileName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+            cursor.close();
+            return fileName;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            cursor.close();
+            return null;
+        }
+    }
+
+    //이미지뷰에 뿌려질 앨범 비트맵 반환
+    public Bitmap getBitmapAlbum( View targetView, Uri uri ) {
+        try {
+            ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
+            if (parcelFileDescriptor == null) return null;
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+            if (fileDescriptor == null) return null;
+
+            int targetW = targetView.getWidth();
+            int targetH = targetView.getHeight();
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+
+            BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
+
+            int photoW = options.outWidth;
+            int photoH = options.outHeight;
+
+            int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+            if (scaleFactor >= 8) {
+                options.inSampleSize = 8;
+            } else if (scaleFactor >= 4) {
+                options.inSampleSize = 4;
+            } else {
+                options.inSampleSize = 2;
+            }
+            options.inJustDecodeBounds = false;
+
+            Bitmap reSizeBit = BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
+
+            ExifInterface exifInterface = null;
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    exifInterface = new ExifInterface(fileDescriptor);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            int exifOrientation;
+            int exifDegree = 0;
+
+            //사진 회전값 구하기
+            if (exifInterface != null) {
+                exifOrientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+                if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+                    exifDegree = 90;
+                } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+                    exifDegree = 180;
+                } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                    exifDegree = 270;
+                }
+            }
+
+            parcelFileDescriptor.close();
+            Matrix matrix = new Matrix();
+            matrix.postRotate(exifDegree);
+
+            Bitmap reSizeExifBitmap = Bitmap.createBitmap(reSizeBit, 0, 0, reSizeBit.getWidth(), reSizeBit.getHeight(), matrix, true);
+            return reSizeExifBitmap;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }//class
--- app/src/main/java/com/cookandroid/swu/Fragment/EboxFragment.java
+++ app/src/main/java/com/cookandroid/swu/Fragment/EboxFragment.java
@@ -1,6 +1,7 @@
 package com.cookandroid.swu.Fragment;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,19 +10,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.cookandroid.swu.EboxAdd;
+import com.cookandroid.swu.ListViewAdapter;
 import com.cookandroid.swu.R;
+
+import java.util.List;
 
 
 public class EboxFragment extends Fragment {
 Button addbutton;
+ListView customListView;
+private static ListViewAdapter listViewAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_ebox, container, false);
+
+        customListView = (ListView) v.findViewById(R.id.listView_custom);
+        listViewAdapter = new ListViewAdapter();
+        customListView.setAdapter(listViewAdapter);
 
         addbutton =(Button) v.findViewById(R.id.addbtn);
         addbutton.setOnClickListener(new View.OnClickListener() {
@@ -33,5 +44,9 @@ Button addbutton;
             }
         });
         return v;
+    }
+    public static void addItem(Bitmap icon, String title, String desc) {
+        listViewAdapter.addItem(icon, title, desc) ;
+        listViewAdapter.notifyDataSetChanged();
     }
 }
