9882969cd314f8f87ae99d3099062a3bc4e8b83a
silvain <syb0424@gmail.com>
2022-11-15 23:02:14 +0900

    camera_com save_com fragment_com adapter_com item_com map_com pharmacy_com location_notcom

--- app/src/main/java/com/cookandroid/swu/Fragment/SearchFragment.java
+++ app/src/main/java/com/cookandroid/swu/Fragment/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.cookandroid.swu.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,7 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.cookandroid.swu.DsSearch;
 import com.cookandroid.swu.R;
 
 public class SearchFragment extends Fragment {
@@ -16,6 +20,16 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+
+        TextView drugstore = v.findViewById(R.id.search_drugstore);
+        drugstore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DsSearch.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 }
