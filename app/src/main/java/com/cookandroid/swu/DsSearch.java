62aabf32f81a2ac2f93eb4f6695643c5c3057bdf
Wonahyeon <wonah10220@gmail.com>
2022-11-29 14:54:41 +0900

    최종시연

--- app/src/main/java/com/cookandroid/swu/DsSearch.java
+++ app/src/main/java/com/cookandroid/swu/DsSearch.java
@@ -62,8 +62,8 @@ public class DsSearch extends AppCompatActivity
 
     static final String TAG = "DsSearch";
     static final int GPS_ENABLE_REQUEST_CODE = 2001;
-    static final int UPDATE_INTERVAL_MS = 1000;  // 1초
-    static final int FASTEST_UPDATE_INTERVAL_MS = 500; // 0.5초
+    static final int UPDATE_INTERVAL_MS = 2000;  // 1초
+    static final int FASTEST_UPDATE_INTERVAL_MS = 1000; // 0.5초
 
     // onRequestPermissionsResult에서 수신된 결과에서
     // ActivityCompat.requestPermissions를 사용한 퍼미션 요청을 구별하기 위해 사용됩니다.
